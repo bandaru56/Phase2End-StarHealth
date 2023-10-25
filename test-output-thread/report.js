0df7cdae9a85ae449c426232fb4e5b384358723a
sandhya <sandhya@gmail.com>
2023-10-25 11:32:18 +0530

    done

---
+++ test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"d078ffe4-7d23-4403-9e15-de1a2df132bb","feature":"Test the starHealth page on Chrome Browser","scenario":"Validate the Star Health Buy Now flow","start":1698212489100,"group":1,"content":"","tags":"","end":1698212489650,"className":"undefined"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
